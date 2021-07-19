b98dd94c411710fa9dd8a1476ca0a215e40cd298
Attapon Peungsook <aonattapon1840.ap@gmail.com>
2021-07-19 16:02:14 +0700

    Add table name m_user

---
+++ src/main/java/com/bearman/demo/backend/entity/User.java
@@ -0,0 +1,22 @@
+package com.bearman.demo.backend.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity(name = "m_user")
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String email;
+
+    @Column(nullable = false, length = 120)
+    private String password;
+
+    @Column(nullable = false, length = 120)
+    private String name;
+}
